20ef0c4114f6ed5bc9a3944e4811f4755d0c2d7b
Rémond Karst <remondkarst@programit.nl>
2016-05-18 14:03:52 +0200

    $1

--- src/main/java/eu/programit/domain/Rémond.java
+++ src/main/java/eu/programit/domain/Rémond.java
@@ -1,3 +1,24 @@
 package eu.programit.domain;
-public class Rémond{
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Rémond extends Person {
+
+	@Override
+	public String getFirstName() {
+		return "Rémond";
+	}
+
+	@Override
+	public List<String> getHobbies() {
+		
+		List<String> result = new ArrayList<>();
+		result.add("running");
+		///
+		
+		return result;	
+	
+	}
+
 }
